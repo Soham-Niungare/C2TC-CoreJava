9229201c191e91ef047f5d3fc5e7e9b0e65a69e1
Soham <sohamn.in@gmail.com>
2024-06-10 19:22:44 +0530

    day-4,5,6 (finished Commit)

---
+++ soham/src/com/day5/Matrix.java
@@ -0,0 +1,27 @@
+package com.day5;
+
+/*
+ * Flow control : Iterative statement
+ * for loop & for-each loop
+ * */
+public class Matrix {
+
+//	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] x = { { 10, 20, 30, 40 }, { 50, 60 } };
+		label1: for (int i = 0; i < x.length; i++) {
+			label2: for (int j = 0; j < x[i].length; j++) {
+				System.out.print(x[i][j] + " ");
+			}
+			System.out.println();
+		}
+		for (int[] x1 : x) {
+			for (int x2 : x1) {
+				System.out.print(x[2] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
